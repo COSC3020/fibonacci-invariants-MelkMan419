9541743ee0b1c3160d006d9ee1e65a8a3c824bc7
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-01-25 12:13:18 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 function fib(n,arr=[]){
     if(n===0){
-        return 0;
+        return [0];
     }
     if(n===1){
-        return 1;
+        return [1];
     }
     if(arr!==undefined){
        return arr[n];
