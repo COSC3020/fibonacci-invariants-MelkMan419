ee35761e044e42ca0dfa16b6b48347b9e1b0247a
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-02-12 14:20:40 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,12 @@
-function fib(n, arr = [0, 1]) {
+function fib(n) {
+    const arr = [0, 1];
     if (n <= 1) {
         return arr.slice(0, n + 1);
     }
-    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
-    return fib(n - 1, arr);
+    for (let i = 2; i <= n; i++) {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+    return arr;
 }
 
 module.exports = { fib };
